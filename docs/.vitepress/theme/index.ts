f95de94de062ee12e334a3ee8662236a0153ea80
wagchi22 <wagchi22@gmail.com>
2026-06-30 23:36:00 -0300

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,5 @@
-// docs/.vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
-import './custom.css' // Importa o CSS do FMHY
 
 export default {
-  extends: DefaultTheme,
-  enhanceApp({ app, router, siteData }) {
-    // Aqui pode adicionar componentes extra se necessário no futuro
-  }
+  extends: DefaultTheme
 }
