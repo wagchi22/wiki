62007f3a4f740f71e9fc2d54f80e3d608a5d7b75
wagchi22 <wagchi22@gmail.com>
2026-09-21 19:54:08 -0300

    fix: corrige link do guia de instalação do TizenTube no Fire TV Android

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
           { text: "Configurando um servidor de mídia", link: "/guias/configurando-um-servidor-de-midia" },
           { text: "Instalando HEVC no Windows 11", link: "/guias/instalando-hevc-no-windows-11" },
           { text: "Diagnosticando rede usando MTR no Windows 11", link: "/guias/diagnosticando-rede-usando-mtr-no-windows-11" },
-          { text: "Instalando TizenTube no Fire TV Android", link: "/guias/instalando-tizentube-no-fire-tv-android" },
+          { text: "Instalando TizenTube no Fire TV Android", link: "/guias/instalando-tizentube-no-firetv-android" },
           { text: "Lista de softwares úteis", link: "/guias/lista-de-softwares-uteis" },
           { text: "Corrigindo miniaturas MP4 no Windows 11", link: "/guias/corrigindo-miniaturas-mp4-no-windows-11" },
           { text: "Gerenciando pacotes com Winget", link: "/guias/gerenciando-pacotes-com-winget" },
